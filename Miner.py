2325f14a6727896b3eee696cbac6199728749e69
w1ndstr5ck <84208375+w1ndstr5ck@users.noreply.github.com>
2023-04-05 15:25:15 -0500

    Removed dif_string

--- Miner.py
+++ Miner.py
@@ -10,17 +10,11 @@ difficulty = 3
 
 def hash(to_hash, max_run = 10000, difficulty = 3):
     for i in range(1, max_run):
-        nonce = i
+        nonce = str(i)
         to_hash = to_hash + nonce
         hash = sha256(to_hash.encode()).hexdigest()
-        if hash[0:difficulty] == dif_string(difficulty):
+        if hash[0:difficulty] == "0" * difficulty:
             return hash
     return "No Hash Found"
-
-def dif_string(difficulty):
-    return_str = ""
-    for i in range(0, difficulty):
-        return_str = return_str + "0"
-    return return_str
 
 print(hash(to_hash, max_run, difficulty))
